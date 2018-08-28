01a9fdcdd10454322f61d9a9c8976bbd74bb9f05
MarineDijoux <dijoux.marine@laposte.net>
2018-08-28 16:18:28 +0200

    test commit marine

--- src/asynchronismAndCommand/Main.java
+++ src/asynchronismAndCommand/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Fn newFn = new Fn();
-
+		// Boucle for
 		for (int i = 0; i < 100; i++) {
 			Command executeFn = new ExecuteFn(newFn);
 			Command stopFn = new StopFn(newFn);
