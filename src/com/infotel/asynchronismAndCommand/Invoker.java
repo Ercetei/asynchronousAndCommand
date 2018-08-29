e76db100eaec69da389604d30d91833087836ea8
MarineDijoux <dijoux.marine@laposte.net>
2018-08-29 16:05:01 +0200

    modif invoker play index

--- src/com/infotel/asynchronismAndCommand/Invoker.java
+++ src/com/infotel/asynchronismAndCommand/Invoker.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 public class Invoker {
 	private List<Command> functions = new ArrayList<Command>();
+	private int currentIndex = 0 ;
 
 	public Invoker() {
 	}
 
+	//Add Function
 	public void store(Command fn) {
 		this.functions.add(fn);
 	}
@@ -18,4 +20,17 @@ public class Invoker {
 			c.execute();
 		}
 	}
+	
+	public void playIndex() {
+		functions.get(currentIndex).execute();
+		next();
+	}
+	
+	public void next() {
+		if (currentIndex < functions.size()) {
+			currentIndex++;
+			playIndex();
+		}
+	}
+	
 }
